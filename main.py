c2fb881abaee3ed365c6ad564087e6602254ac74
Oluwatobi Onasanya <onatty98@gmail.com>
2021-03-19 17:24:06 +0000

    example 4

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import datetime
 from flask import Flask, render_template
 from google.cloud import datastore
+import google.oauth2.id_token
+from flask import Flask, render_template, request
+from google.auth.transport import requests
 
 app = Flask(__name__)
 datastore_client = datastore.Client()
+firebase_request_adapter = requests.Request()
 
 
 def store_time(dt):
@@ -21,10 +25,23 @@ def fetch_times(limit):
 
 @app.route('/')
 def root():
-    store_time(datetime.datetime.now())
-    times = fetch_times(10)
-    print(times)
-    return render_template('index.html', times=times)
+    # query firebase for the request token and set other variables to none for now
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    times = None
+
+    # if we have an ID token then verify it against firebase if it doesn't check out then
+    # log the error message that is returned
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+        except ValueError as exc:
+            error_message = str(exc)
+
+    # render the template with the last times we have
+    return render_template('index.html', user_data=claims, error_message=error_message)
+
 
 
 if __name__ == '__main__':
---
+++ static/app-setup.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBW9sF4-s5LWC3hGKgcQNuSdx0ubI2t488",
+    authDomain: "test-555-308115.firebaseapp.com",
+    projectId: "test-555-308115",
+    storageBucket: "test-555-308115.appspot.com",
+    messagingSenderId: "473707658334",
+    appId: "1:473707658334:web:d194c986bf43c87f9208cf",
+    measurementId: "G-LGMCBYQZ8K"
+    };
+    firebase.initializeApp(firebaseConfig);
