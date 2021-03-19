35a07a74285152ece00518e7dd6fb65c6890bf75
Oluwatobi Onasanya <onatty98@gmail.com>
2021-03-19 16:14:29 +0000

    example 3

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import datetime
 from flask import Flask, render_template
+from google.cloud import datastore
+
 app = Flask(__name__)
+datastore_client = datastore.Client()
+
+
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({'timestamp': dt})
+    datastore_client.put(entity)
+
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['timestamp']
+    times = query.fetch()
+    return times
+
+
 @app.route('/')
 def root():
- dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
- datetime.datetime(2018, 1, 2, 10, 30, 0),
- datetime.datetime(2018, 1, 3, 11, 0, 0),
- ]
- return render_template('index.html', times=dummy_times)
+    store_time(datetime.datetime.now())
+    times = fetch_times(10)
+    print(times)
+    return render_template('index.html', times=times)
+
+
 if __name__ == '__main__':
- app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
