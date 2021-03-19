60340cf90996d1f667066e986dcd7b87ab4ca0e5
Oluwatobi Onasanya <onatty98@gmail.com>
2021-03-19 18:44:08 +0000

    example 6

--- main.py
+++ main.py
@@ -1,13 +1,82 @@
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
 from google.cloud import datastore
 import google.oauth2.id_token
 from flask import Flask, render_template, request
 from google.auth.transport import requests
-
 app = Flask(__name__)
 datastore_client = datastore.Client()
 firebase_request_adapter = requests.Request()
+
+
+@app.route('/edit_user_info', methods=['POST'])
+def editUserInfo():
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    user_info = None
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(id_token,
+                                                                firebase_request_adapter)
+            new_string = request.form['string_update']
+            new_int = request.form['int_update']
+            new_float = request.form['float_update']
+            updateUserInfo(claims, new_string, new_int, new_float)
+        except ValueError as exc:
+            error_message = str(exc)
+    return redirect("/")
+
+
+def updateUserInfo(claims, new_string, new_int, new_float):
+    entity_key = datastore_client.key('UserInfo', claims['email'])
+    entity = datastore_client.get(entity_key)
+    entity.update({
+        'string_value': new_string,
+        'int_value': new_int,
+        'float_value': new_float
+    })
+    datastore_client.put(entity)
+
+
+def createUserInfo(claims):
+    entity_key = datastore_client.key('UserInfo', claims['email'])
+    entity = datastore.Entity(key=entity_key)
+    entity.update({
+        'email': claims['email'],
+        'name': claims['name'],
+        'creation_date': datetime.datetime.now(),
+        'bool_value': True,
+        'float_value': 3.14,
+        'int_value': 10,
+        'string_value': 'this is a sample string',
+        'list_value': [1, 2, 3, 4],
+        'dictionary_value': {'A': 1, 'B': 2, 'C': 3}
+    })
+    datastore_client.put(entity)
+
+
+def createUserInfo(claims):
+    entity_key = datastore_client.key('UserInfo', claims['email'])
+    entity = datastore.Entity(key=entity_key)
+    entity.update({
+        'email': claims['email'],
+        'name': claims['name'],
+        'creation_date': datetime.datetime.now(),
+        'bool_value': True,
+        'float_value': 3.14,
+        'int_value': 10,
+        'string_value': 'this is a sample string',
+        'list_value': [1, 2, 3, 4],
+        'dictionary_value': {'A': 1, 'B': 2, 'C': 3}
+    })
+    datastore_client.put(entity)
+
+
+def retrieveUserInfo(claims):
+    entity_key = datastore_client.key('UserInfo', claims['email'])
+    entity = datastore_client.get(entity_key)
+    return entity
 
 
 def store_time(email, dt):
@@ -20,33 +89,29 @@ def fetch_times(email, limit):
     ancestor_key = datastore_client.key('User', email)
     query = datastore_client.query(kind='visit', ancestor=ancestor_key)
     query.order = ['-timestamp']
-    times =  query.fetch(limit=limit)
+    times = query.fetch(limit=limit)
     # print(times,ancestor_key)
     return times
 
 
 @app.route('/')
 def root():
-    # query firebase for the request token and set other variables to none for now
     id_token = request.cookies.get("token")
     error_message = None
     claims = None
-    times = None
-
-    # if we have an ID token then verify it against firebase if it doesn't check out then
-    # log the error message that is returned
+    user_info = None
     if id_token:
         try:
             claims = google.oauth2.id_token.verify_firebase_token(id_token,
-            firebase_request_adapter)
-            store_time(claims['email'], datetime.datetime.now())
-            times = fetch_times(claims['email'], 10)
-        
+                                                                firebase_request_adapter)
+            user_info = retrieveUserInfo(claims)
+            if user_info == None:
+                createUserInfo(claims)
+                user_info = retrieveUserInfo(claims)
         except ValueError as exc:
             error_message = str(exc)
-
-    # render the template with the last times we have
-    return render_template('index.html', user_data=claims, error_message=error_message,times=times)
+    return render_template('index.html', user_data=claims, error_message=error_message,
+                           user_info=user_info)
 
 
 if __name__ == '__main__':
